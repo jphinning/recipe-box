6b86df9d7b98549350d432bc6b2c19898739e56e
Joao Hinning <jphinning@gmail.com>
2022-09-24 16:55:49 -0300

    Recipes connected to Users

--- packages/server/src/modules/User/userType.ts
+++ packages/server/src/modules/User/userType.ts
@@ -1,17 +1,33 @@
 import { GraphQLObjectType, GraphQLString } from 'graphql';
-import { connectionDefinitions, globalIdField } from 'graphql-relay';
+import {
+  connectionArgs,
+  connectionDefinitions,
+  connectionFromArray,
+  globalIdField,
+} from 'graphql-relay';
 import { nodeInterface } from '../../graphql/nodeInterface';
+import { RecipesModel } from '../Recipes/recipesModel';
+import { RecipesConnection } from '../Recipes/recipesType';
 import { IUser } from './userModel';
 
 export const UserType: GraphQLObjectType<IUser> = new GraphQLObjectType<IUser>({
   name: 'User',
-  description: 'A user who loves to run',
   interfaces: [nodeInterface],
   fields: () => ({
     id: globalIdField(),
     fullName: { type: GraphQLString, description: 'The name of the user' },
     email: { type: GraphQLString },
     password: { type: GraphQLString },
+    recipes: {
+      type: RecipesConnection,
+      description: 'All user recipes',
+      args: connectionArgs,
+      resolve: async (user, args) => {
+        const userRecipes = await RecipesModel.find({ userId: user._id });
+
+        return connectionFromArray([...userRecipes], args);
+      },
+    },
   }),
 });
 
--- packages/server/src/schema/mutation.ts
+++ packages/server/src/schema/mutation.ts
@@ -1,4 +1,5 @@
 import { GraphQLObjectType } from 'graphql';
+import { recipeMutations } from '../modules/Recipes/RecipesMutations/recipesMutations';
 import { userMutations } from '../modules/User/UserMutations/userMutations';
 
 export const mutation = new GraphQLObjectType({
@@ -6,5 +7,6 @@ export const mutation = new GraphQLObjectType({
   description: 'RootMutationType',
   fields: {
     ...userMutations,
+    ...recipeMutations,
   },
 });
