834e140ee39d11825229ba69b176cb880fdbb1c9
Joao Hinning <jphinning@gmail.com>
2022-09-29 11:14:36 -0300

    feat: Heroku deploy config created

--- packages/server/src/database/dbConnection.ts
+++ packages/server/src/database/dbConnection.ts
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose';
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { NODE_ENV, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, DB_PROD_URI } =
+  process.env;
 
 export const connectToDB = async () => {
   mongoose.connection
     .on('error', (err) => console.error(err))
     .once('open', () => console.log('Mongo up and running'));
 
-  await mongoose.connect(
-    `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-  );
+  if (NODE_ENV === 'dev') {
+    await mongoose.connect(
+      `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+    );
+
+    return;
+  }
+
+  await mongoose.connect(DB_PROD_URI!);
 };
