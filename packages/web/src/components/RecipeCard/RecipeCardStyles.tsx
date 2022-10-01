ab170a716952c235c3d283be9dbb3d56391286a5
Joao Hinning <jphinning@gmail.com>
2022-10-01 00:52:41 -0300

    Post details page added

--- packages/web/src/components/RecipeCard/RecipeCardStyles.tsx
+++ packages/web/src/components/RecipeCard/RecipeCardStyles.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { Box } from '@mui/system';
-import { WHITE } from '../../utils/colorConsts';
+import { Link } from 'react-router-dom';
+import { BLACK, BLUE, WHITE } from '../../utils/colorConsts';
 
 export const RecipeWrapper = styled(Box)`
   display: flex;
@@ -13,6 +14,24 @@ export const RecipeWrapper = styled(Box)`
   box-shadow: rgba(0, 0, 0, 0.05) 0px 2px 4px;
   row-gap: 15px;
   overflow-wrap: break-word;
+`;
+
+export const RouterLink = styled(Link)`
+  overflow: hidden;
+  text-decoration: 'none' !important;
+  color: ${BLACK};
+
+  &:focus,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+
+  &:hover {
+    text-decoration: none;
+    color: ${BLUE};
+  }
 `;
 
 export const CardHeader = styled.div`
